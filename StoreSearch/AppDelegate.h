57ce0bb6547cb18514b2428b4122373cff3fd5c9
zld <zhu.lida.cn@gmail.com>
2014-11-04 15:52:05 +0800

    init

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by 朱里达 on 14/11/2.
+//  Copyright (c) 2014年 zld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class SearchViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) SearchViewController *searchViewController;
+
+@end
+
---
+++ StoreSearch/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  StoreSearch
+//
+//  Created by 朱里达 on 14/11/4.
+//  Copyright (c) 2014年 zld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
