52771f37df822b898bc6ba4b5281d6cd20ba06d2
zld <zhu.lida.cn@gmail.com>
2014-11-05 13:55:11 +0800

    search bar and table view added, fake result

---
+++ StoreSearch/SearchResult.h
@@ -0,0 +1,16 @@
+//
+//  SearchResult.h
+//  StoreSearch
+//
+//  Created by 朱里达 on 14/11/5.
+//  Copyright (c) 2014年 zld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SearchResult : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *artistName;
+
+@end
---
+++ StoreSearch/SearchResult.m
@@ -0,0 +1,13 @@
+//
+//  SearchResult.m
+//  StoreSearch
+//
+//  Created by 朱里达 on 14/11/5.
+//  Copyright (c) 2014年 zld. All rights reserved.
+//
+
+#import "SearchResult.h"
+
+@implementation SearchResult
+
+@end
